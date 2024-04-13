2f0061b0a54d91b4e7f8d2b2af83116ee8ff1dd6
abhay9656 <abhay9656@gmail.com>
2024-04-13 21:59:09 +0530

    created a about us page

--- frontend/src/app/(main)/about/page.jsx
+++ frontend/src/app/(main)/about/page.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
-
+import styles from './about.module.css'
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <>
+  <header className={styles.header}>
+    <h1>About Us</h1>
+  </header>
+  <main className={styles.main}>
+    <section>
+      <h2  className='text-3xl font-bold mb-2'>Our Company</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis
+        volutpat magna. Nam eget dui eu ex malesuada finibus. Nullam rutrum
+        turpis vitae ante fringilla, et fermentum mi ultrices. Fusce dignissim
+        ligula quis justo euismod, sit amet mollis lectus convallis. Sed aliquet
+        eros et sapien suscipit, vel sollicitudin velit semper. Vivamus
+        ullamcorper risus in eros vestibulum ultrices.
+      </p>
+    </section>
+    <section >
+      <h2 className='text-3xl font-bold my-4'>Our Team</h2>
+     <div className='list-none'>
+      <li>Privacy</li>
+      <li>Features</li>
+     </div>
+    </section>
+  </main>
+  <footer className={styles.footer}>
+    <p>© 2024 Company Name. All rights reserved.</p>
+  </footer>
+</>
+
+    </div>
   )
 }
 
