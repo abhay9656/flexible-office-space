4deab57f8555cd0779c130d7995de24c5b7ed35f
abhay9656 <abhay9656@gmail.com>
2024-04-13 13:01:37 +0530

    created a login page

--- frontend/src/app/(main)/login/page.jsx
+++ frontend/src/app/(main)/login/page.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-
+import styles from './page.module.css'
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <div className={styles.main}>
+        <div className=>left</div>
+        <div className=' w-3/5 bg-gray-500 right'>right</div>
+      </div>
+    </div>
   )
 }
 
