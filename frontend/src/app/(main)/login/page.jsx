f45c5e49d2790d10205c4d5a9ce1aab258818997
abhay9656 <abhay9656@gmail.com>
2024-04-13 13:31:04 +0530

    stying the login page

--- frontend/src/app/(main)/login/page.jsx
+++ frontend/src/app/(main)/login/page.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import styles from './page.module.css'
 const Login = () => {
   return (
-    <div>
+    <div style={{height:'100vh'}} className='flex justify-center items-center'>
       <div className={styles.main}>
-        <div className=>left</div>
-        <div className=' w-3/5 bg-gray-500 right'>right</div>
+        <div className={`w-3/5 bg-gray-500 ${styles.left}`}>
+          <div className={`${styles.content}`}>
+                 <h2 className='pt-1 text-center text-2xl'>Welcome Back</h2>
+                 <button className={`mt-4 ml-2  ${styles.btn}`}>Login With Google</button>
+          </div>
+        </div>
+        <div className={`w-3/5  ${styles.left}`}>
+          <img className={styles.img} src="./sign.png" alt="" />
+        </div>
       </div>
     </div>
   )
