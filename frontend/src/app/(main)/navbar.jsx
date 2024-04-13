3c5b71423bcf4b2c55b8de51dae30a741d1cd9c1
abhay9656 <abhay9656@gmail.com>
2024-04-13 12:39:22 +0530

    navigation bar is created by using tailwind css

--- frontend/src/app/(main)/navbar.jsx
+++ frontend/src/app/(main)/navbar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='bg-green-500'>
-      <div>LOGO</div>
-      <div>
+    <nav className='bg-green-500 flex justify-between h-16 items-center list-none'>
+      <div className='ml-2'>LOGO</div>
+      <div className='flex mr-2 gap-5'>
         <li>Home</li>
         <li>About</li>
         <li>Contact</li>
