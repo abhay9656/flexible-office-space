b0a8824832033488f6210410429f6e13d12125e6
abhay9656 <abhay9656@gmail.com>
2024-04-13 12:31:30 +0530

    created a navbar

--- frontend/src/app/(main)/navbar.jsx
+++ frontend/src/app/(main)/navbar.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav>
-      
+    <nav className='bg-green-500'>
+      <div>LOGO</div>
+      <div>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Login</li>
+        <li>Sign Up</li>
+      </div>
     </nav>
   )
 }
