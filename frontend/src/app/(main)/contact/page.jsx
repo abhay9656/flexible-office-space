0a42ab2dc0fc24d1a6303b6f931ab3a586181772
abhay9656 <abhay9656@gmail.com>
2024-04-13 22:11:59 +0530

    created a contact us page

--- frontend/src/app/(main)/contact/page.jsx
+++ frontend/src/app/(main)/contact/page.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import styles from './contact.module.css'
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className={styles.container}>
+    <main className={styles.main}>
+      <h1 className={styles.title}>Contact Us</h1>
+      <p className={styles.description}>Have questions or feedback? We'd love to hear from you!</p>
+
+      <div className={styles.form}>
+        <label>Name:</label>
+        <input className={styles.input} type="text"  />
+
+        <labeel>Email:</labeel>
+        <input className={styles.input} type="email"  />
+
+        <label >Message:</label>
+        <textarea className={styles.textarea}></textarea>
+
+        <button className={styles.btn} type="submit">Submit</button>
+      </div>
+    </main>
+  </div>
   )
 }
 
