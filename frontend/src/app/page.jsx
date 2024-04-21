6b0a21f013c5f48d7129697480abe2f20e0e75dd
abhay9656 <abhay9656@gmail.com>
2024-04-21 15:28:12 +0530

    make changes

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -10,12 +10,10 @@ const Home = () => {
         <div className="flex items-center w-full lg:w-1/2">
           <div className="max-w-2xl mb-8">
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
-              Free Landing Page Template for startups
+              We're the perfect Woring Space For you
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
-              Nextly is a free landing page & marketing website
-              template for startups and indie projects. Its built with
-              Next.js & TailwindCSS. And its completely open-source.
+            A coworking Space for your lifeStyle
             </p>
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
