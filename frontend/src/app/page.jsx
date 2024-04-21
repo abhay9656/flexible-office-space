f5128b8b8d3d19f812e078af382138117aa637dc
abhay9656 <abhay9656@gmail.com>
2024-04-21 16:20:30 +0530

    added feature page

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './(main)/navbar'
+// import { FaWifi } from "react-icons/fa";
 
 const Home = () => {
   return (
@@ -10,7 +11,7 @@ const Home = () => {
         <div className="flex items-center w-full lg:w-1/2">
           <div className="max-w-2xl mb-8">
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
-              We're the perfect Woring Space For you
+              We're the perfect Working Space For you
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
             A coworking Space for your lifeStyle
@@ -66,8 +67,276 @@ const Home = () => {
             customers worldwide
           </div>
         </div>
+
       </div>
-    
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="grid gap-10 lg:grid-cols-2">
+        <div className="flex flex-col justify-center md:pr-8 xl:pr-0 lg:max-w-lg">
+          <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-teal-accent-400">
+            <svg className="text-teal-900 w-7 h-7" viewBox="0 0 24 24">
+              <polyline
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                points=" 8,5 8,1 16,1 16,5"
+                strokeLinejoin="round"
+              />
+              <polyline
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                points="9,15 1,15 1,5 23,5 23,15 15,15"
+                strokeLinejoin="round"
+              />
+              <polyline
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                points="22,18 22,23 2,23 2,18"
+                strokeLinejoin="round"
+              />
+              <rect
+                x="9"
+                y="13"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                width="6"
+                height="4"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </div>
+          <div className="max-w-xl mb-6">
+            <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+             Discover the Comfort of Our{' '}
+              {/* <br className="hidden md:block" /> */}
+              
+              <span className="inline-block text-deep-purple-accent-400 text-blue-700">
+               Welcoming Spaces
+              </span>
+            </h2>
+            <p className="text-base text-gray-700 md:text-lg">
+              Explore the different types of office space available to rentand discover which is right for your business.
+            </p>
+          </div>
+          <div>
+            <a
+              href="/"
+              aria-label=""
+              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              Learn more
+              <svg
+                className="inline-block w-3 ml-2"
+                fill="currentColor"
+                viewBox="0 0 12 12"
+              >
+                <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
+              </svg>
+            </a>
+          </div>
+        </div>
+        <div className="flex items-center justify-center -mx-4 lg:pl-8">
+          <div className="flex flex-col items-end px-3">
+            <img
+              className="object-cover mb-6 rounded shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56"
+              src="https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+              alt=""
+            />
+            <img
+              className="object-cover w-20 h-20 rounded shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40"
+              src="https://images.pexels.com/photos/3182812/pexels-photo-3182812.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+              alt=""
+            />
+          </div>
+          <div className="px-3">
+            <img
+              className="object-cover w-40 h-40 rounded shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80"
+              src="https://images.pexels.com/photos/3182739/pexels-photo-3182739.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    {/* features */}
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+        {/* <div>
+          <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+            Brand new
+          </p>
+        </div> */}
+        <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+          <span className="relative inline-block">
+            <svg
+              viewBox="0 0 52 24"
+              fill="currentColor"
+              className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+            >
+              <defs>
+                <pattern
+                  id="18302e52-9e2a-4c8e-9550-0cbb21b38e55"
+                  x="0"
+                  y="0"
+                  width=".135"
+                  height=".30"
+                >
+                  <circle cx="1" cy="1" r=".7" />
+                </pattern>
+              </defs>
+              <rect
+                fill="url(#18302e52-9e2a-4c8e-9550-0cbb21b38e55)"
+                width="52"
+                height="24"
+              />
+            </svg>
+            <span className="relative">We</span>
+          </span>{' '}
+         Provide more than just a Space
+        </h2>
+        {/* <p className="text-base text-gray-700 md:text-lg">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque rem aperiam, eaque ipsa quae.
+        </p> */}
+      </div>
+      <div className="grid gap-4 row-gap-5 sm:grid-cols-2 lg:grid-cols-4">
+        <div className="flex flex-col justify-between p-5 border rounded shadow-sm">
+          <div>
+            <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+              {/* <svg
+                className="w-12 h-12 text-deep-purple-accent-400"
+                stroke="currentColor"
+                viewBox="0 0 52 52"
+              >
+                <polygon
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fill="none"
+                  points="29 13 14 29 25 29 23 39 38 23 27 23"
+                />
+              </svg> */}
+              {/* <FaWifi /> */}
+            </div>
+            <h6 className="mb-2 font-semibold leading-5">High Speed Internet</h6>
+            <p className="mb-3 text-sm text-gray-900">
+              A flower in my garden, a mystery in my panties. Heart attack never
+              stopped old Big Bear.
+            </p>
+          </div>
+          <a
+            href="/"
+            aria-label=""
+            className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+          >
+            Learn more
+          </a>
+        </div>
+        <div className="flex flex-col justify-between p-5 border rounded shadow-sm">
+          <div>
+            <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+              <svg
+                className="w-12 h-12 text-deep-purple-accent-400"
+                stroke="currentColor"
+                viewBox="0 0 52 52"
+              >
+                <polygon
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fill="none"
+                  points="29 13 14 29 25 29 23 39 38 23 27 23"
+                />
+              </svg>
+            </div>
+            <h6 className="mb-2 font-semibold leading-5">When has justice</h6>
+            <p className="mb-3 text-sm text-gray-900">
+              Rough pomfret lemon shark plownose chimaera southern sandfish
+              kokanee northern sea.
+            </p>
+          </div>
+          <a
+            href="/"
+            aria-label=""
+            className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+          >
+            Learn more
+          </a>
+        </div>
+        <div className="flex flex-col justify-between p-5 border rounded shadow-sm">
+          <div>
+            <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+              <svg
+                className="w-12 h-12 text-deep-purple-accent-400"
+                stroke="currentColor"
+                viewBox="0 0 52 52"
+              >
+                <polygon
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fill="none"
+                  points="29 13 14 29 25 29 23 39 38 23 27 23"
+                />
+              </svg>
+            </div>
+            <h6 className="mb-2 font-semibold leading-5">Organically grow</h6>
+            <p className="mb-3 text-sm text-gray-900">
+              A slice of heaven. O for awesome, this chocka full cuzzie is as
+              rip-off as a cracker.
+            </p>
+          </div>
+          <a
+            href="/"
+            aria-label=""
+            className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+          >
+            Learn more
+          </a>
+        </div>
+        <div className="flex flex-col justify-between p-5 border rounded shadow-sm">
+          <div>
+            <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+              <svg
+                className="w-12 h-12 text-deep-purple-accent-400"
+                stroke="currentColor"
+                viewBox="0 0 52 52"
+              >
+                <polygon
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fill="none"
+                  points="29 13 14 29 25 29 23 39 38 23 27 23"
+                />
+              </svg>
+            </div>
+            <h6 className="mb-2 font-semibold leading-5">A slice of heaven</h6>
+            <p className="mb-3 text-sm text-gray-900">
+              Disrupt inspire and think tank, social entrepreneur but
+              preliminary thinking think tank compelling.
+            </p>
+          </div>
+          <a
+            href="/"
+            aria-label=""
+            className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+          >
+            Learn more
+          </a>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
