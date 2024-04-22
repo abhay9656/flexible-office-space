24f48358ec7e9be7467555b7f8b3957c3db1b766
abhay9656 <abhay9656@gmail.com>
2024-04-22 21:56:15 +0530

    make change in about page,contact page

--- frontend/src/app/(main)/about/page.jsx
+++ frontend/src/app/(main)/about/page.jsx
@@ -1,39 +1,47 @@
-import React from 'react'
-import styles from './about.module.css'
+import React from "react";
+import styles from "./about.module.css";
 const About = () => {
   return (
     <div>
       <>
-  <header className={`bg-blue-700 ${styles.header}`}>
-    <h1>About Us</h1>
-  </header>
-  <main className={styles.main}>
-    <section>
-      <h2  className='text-3xl font-bold mb-2'>Our Company</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis
-        volutpat magna. Nam eget dui eu ex malesuada finibus. Nullam rutrum
-        turpis vitae ante fringilla, et fermentum mi ultrices. Fusce dignissim
-        ligula quis justo euismod, sit amet mollis lectus convallis. Sed aliquet
-        eros et sapien suscipit, vel sollicitudin velit semper. Vivamus
-        ullamcorper risus in eros vestibulum ultrices.
-      </p>
-    </section>
-    <section >
-      <h2 className='text-3xl font-bold my-4'>Our Team</h2>
-     <div className='list-none'>
-      <li>Privacy</li>
-      <li>Features</li>
-     </div>
-    </section>
-  </main>
-  <footer className={`bg-blue-700 ${styles.footer}`}>
-    <p>© 2024 office Space. All rights reserved.</p>
-  </footer>
-</>
-
+        <header className={`bg-blue-700 ${styles.header}`}>
+          <h1>About Us</h1>
+        </header>
+        <main className={styles.main}>
+          <section>
+            <h2 className="text-3xl font-bold mb-2">
+              Our<span className="text-blue-700">Company</span>
+            </h2>
+            <p>
+              At Office Space, we're dedicated to revolutionizing the way you
+              work. We understand that the traditional office model doesn't
+              always suit the diverse needs of modern businesses and
+              professionals. That's why we've crafted a flexible office space
+              solution that adapts to you.
+            </p>
+          </section>
+          <section>
+            <h2 className="text-3xl font-bold my-4">Our<span className="text-blue-700">Mission</span></h2>
+            <p>
+              Our mission is simple: to provide a dynamic workspace that fosters
+              productivity, collaboration, and innovation. We believe that where
+              you work should empower you to do your best work, whether you're a
+              freelancer, startup, or established corporation.
+            </p>
+          </section>
+          <section>
+            <h2 className="text-3xl font-bold my-4">Our<span className="text-blue-700">Spaces</span></h2>
+            <p>
+            From private offices and dedicated desks to shared coworking areas and meeting rooms, we have a space that's perfect for you. Our thoughtfully designed environments are equipped with high-speed internet, ergonomic furniture, and everything else you need to thrive.
+            </p>
+          </section>
+        </main>
+        <footer className={`bg-blue-700 ${styles.footer}`}>
+          <p>© 2024 office Space. All rights reserved.</p>
+        </footer>
+      </>
     </div>
-  )
-}
+  );
+};
 
 export default About;
