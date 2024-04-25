38c30d8643e5ec25b65ce2866653967b714480f0
abhay9656 <abhay9656@gmail.com>
2024-04-25 10:13:09 +0530

    make changes

--- frontend/src/app/(main)/navbar.jsx
+++ frontend/src/app/(main)/navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Office<span className="text-blue-700">Spcae</span>
+            Office<span className="text-blue-700">Space</span>
           </span>
         </a>
         <button
--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -24,7 +24,7 @@ const Home = () => {
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
               <a
-                href="https://web3templates.com/templates/nextly-landing-page-template-for-startups"
+                href="#"
                 target="_blank"
                 rel="noopener"
                 className="px-5 py-3 ml-3 text-lg font-medium text-center text-white bg-indigo-600 rounded-md ">
@@ -35,7 +35,7 @@ const Home = () => {
                 target="_blank"
                 rel="noopener"
                 className="flex items-center space-x-2 text-gray-500 dark:text-gray-400">
-               <FaInstagram />
+               <FaInstagram size={30} />
                 <span> View on Intagram</span>
               </a>
             </div>
