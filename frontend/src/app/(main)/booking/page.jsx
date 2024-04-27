d8147c78e67a561b743dafe9de03309514d81b25
abhay9656 <abhay9656@gmail.com>
2024-04-27 22:06:04 +0530

    created a booking page

---
+++ frontend/src/app/(main)/booking/page.jsx
@@ -0,0 +1,89 @@
+"use client";
+import React from 'react'
+import { useFormik } from "formik";
+
+const booking = () => {
+
+    const book=useFormik({
+        initialValues:{
+            area:'',
+            duration:'',
+            bookingdate:'',
+            price:''
+        },
+        onSubmit:(values)=>{
+            console.log(values);
+        }
+    })
+
+  return (
+    <div>
+      <div className="container mx-auto px-4">
+      <h1 className="text-2xl text-center font-bold mb-4">Booking Page</h1>
+      <form onSubmit={book.handleSubmit}>
+        <div className="mb-4">
+          <label htmlFor="area" className="block text-sm font-medium text-gray-700">
+            Area
+          </label>
+          <input
+            type="text"
+            id="area"
+            className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+            onChange={book.handleChange}
+            value={book.values.area}
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="duration" className="block text-sm font-medium text-gray-700">
+            Duration
+          </label>
+          <input
+            type="text"
+            id="duration"
+            className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+            onChange={book.handleChange}
+            value={book.values.duration}
+            
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="bookingDate" className="block text-sm font-medium text-gray-700">
+            Booking Date
+          </label>
+          <input
+            type="date"
+            id="bookingDate"
+            className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+            onChange={book.handleChange}
+            value={book.values.bookingdate}
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="price" className="block text-sm font-medium text-gray-700">
+            Price
+          </label>
+          <input
+            type="text"
+            id="price"
+            className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+            onChange={book.handleChange}
+            value={book.values.price}
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+        >
+          Book Now
+        </button>
+      </form>
+    </div>
+    </div>
+  )
+}
+
+export default booking
