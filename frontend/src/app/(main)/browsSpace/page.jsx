310dcc9f05da690e58687e8f97f3bf997ce14a7f
abhay9656 <abhay9656gupta@gmail.com>
2024-05-01 20:35:41 +0530

    makes changes in navbar

--- frontend/src/app/(main)/browsSpace/page.jsx
+++ frontend/src/app/(main)/browsSpace/page.jsx
@@ -10,36 +10,36 @@ const browseSpace = () => {
             <div className="max-w-xs cursor-pointer rounded-lg bg-white p-2 shadow duration-150 hover:scale-105 hover:shadow-md">
               <img
                 className="w-full rounded-lg object-cover object-center"
-                src="https://images.unsplash.com/photo-1580477371194-4593e3c7c6cf?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                src="https://www.wework.com/ideas/wp-content/uploads/sites/4/2021/08/20201008-199WaterSt-2_fb.jpg"
                 alt="product"
               />
               <div>
                 <div className="my-6 flex items-center justify-between px-4">
-                  <p className="font-bold text-gray-500">Space</p>
+                  <p className="font-bold text-gray-500">Space Detail</p>
                   
                 </div>
                 <div className="my-4 flex items-center justify-between px-4">
-                  <p className="text-sm font-semibold text-gray-500">First option</p>
+                  <p className="text-sm font-semibold text-gray-500">Area</p>
                   <p className="rounded-full bg-gray-200 px-2 py-0.5 text-xs font-semibold text-gray-600">
-                    23
+                    2300 sq
                   </p>
                 </div>
                 <div className="my-4 flex items-center justify-between px-4">
-                  <p className="text-sm font-semibold text-gray-500">Second option</p>
+                  <p className="text-sm font-semibold text-gray-500">Duration</p>
                   <p className="rounded-full bg-gray-200 px-2 py-0.5 text-xs font-semibold text-gray-600">
-                    7
+                    7 Days
                   </p>
                 </div>
                 <div className="my-4 flex items-center justify-between px-4">
-                  <p className="text-sm font-semibold text-gray-500">Third option</p>
+                  <p className="text-sm font-semibold text-gray-500">price</p>
                   <p className="rounded-full bg-gray-200 px-2 py-0.5 text-xs font-semibold text-gray-600">
-                    1
+                    4000
                   </p>
                 </div>
                 <div className="my-4 flex items-center justify-between px-4">
-                  <p className="text-sm font-semibold text-gray-500">Fourth option</p>
+                  <p className="text-sm font-semibold text-gray-500">Date</p>
                   <p className="rounded-full bg-gray-200 px-2 py-0.5 text-xs font-semibold text-gray-600">
-                    23
+                    23/04/2024
                   </p>
                 </div>
               </div>
--- frontend/src/app/(main)/navbar.jsx
+++ frontend/src/app/(main)/navbar.jsx
@@ -61,12 +61,20 @@ const Navbar = () => {
               </Link>
             </li>
             <li>
-              <a
+              <Link
                 href="/login"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Login
-              </a>
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/browsSpace"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Browse Space
+              </Link>
             </li>
             <li>
               <Link
@@ -76,14 +84,7 @@ const Navbar = () => {
                 Sign up
               </Link>
             </li>
-            <li>
-              <Link
-                href="/booking"
-                className="block  bg-blue-700 text-white rounded hover:bg-blue-700  md:border-0 md:hover:text-white md:p-1 dark:text-white md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Add Space
-              </Link>
-            </li>
+        
           </ul>
         </div>
       </div>
