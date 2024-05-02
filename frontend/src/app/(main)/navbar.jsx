8a6aa5ec65f6fd9711ee3f0b08ebe49bf204b2f4
abhay9656 <abhay9656gupta@gmail.com>
2024-05-02 17:42:02 +0530

    created a new page feedback

--- frontend/src/app/(main)/navbar.jsx
+++ frontend/src/app/(main)/navbar.jsx
@@ -78,6 +78,14 @@ const Navbar = () => {
             </li>
             <li>
               <Link
+                href="/feedback"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Feedback
+              </Link>
+            </li>
+            <li>
+              <Link
                 href="/signup"
                 className="block  bg-blue-700 text-white rounded hover:bg-blue-700  md:border-0 md:hover:text-white md:p-1 dark:text-white md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
