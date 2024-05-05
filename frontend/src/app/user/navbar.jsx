98cdf5c589bbbd6ed09ea9c57a630aadb9a94167
abhay9656 <abhay9656gupta@gmail.com>
2024-05-05 12:44:00 +0530

    added loction to navbar

--- frontend/src/app/user/navbar.jsx
+++ frontend/src/app/user/navbar.jsx
@@ -12,7 +12,7 @@ return (<div>
           Office<span className="text-blue-700">Space</span>
         </a>
         <div className="flex flex-row items-center gap-5 mt-5 sm:justify-end sm:mt-0 sm:ps-5">
-          <a className="font-medium text-blue-500" href="#" aria-current="page">
+          <a className="font-medium text-blue-500" href="/user/booking" aria-current="page">
             Booking
           </a>
           <a
