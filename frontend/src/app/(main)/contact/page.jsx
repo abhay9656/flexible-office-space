9f85246bc97bd4cdb6bd5c8e071a15447598cc7a
abhay9656 <abhay9656gupta@gmail.com>
2024-05-07 19:02:57 +0530

    chnages manageUser page

--- frontend/src/app/(main)/contact/page.jsx
+++ frontend/src/app/(main)/contact/page.jsx
@@ -1,24 +1,43 @@
+'use client'
 import React from 'react'
 import styles from './contact.module.css'
+import { useFormik } from 'formik'
 const Contact = () => {
+
+  const contact =useFormik({
+    initialValues:{
+      name:'',
+      email:'',
+      message:''
+    },
+    onSubmit:(values)=>{
+      console.log(values);
+    }
+  })
+
   return (
     <div className={styles.container}>
     <main className={styles.main}>
       <h1 className={styles.title}>Contact<span className='text-blue-700'>Us</span></h1>
       <p className={styles.description}>Have questions or feedback? We'd love to hear from you!</p>
-
+      <form action="" onSubmit={contact.handleSubmit}>
       <div className={styles.form}>
         <label>Name:</label>
-        <input className={styles.input} type="text"  />
+        <input className={styles.input} id='name' type="text"  onChange={contact.handleChange}
+        value={contact.values.name}/>
 
-        <labeel>Email:</labeel>
-        <input className={styles.input} type="email"  />
+        <label>Email:</label>
+        <input className={styles.input} id='email ' type="email" onChange={contact.handleChange}
+          value={contact.values.email} />
 
         <label >Message:</label>
-        <textarea className={styles.textarea}></textarea>
+        <textarea id='message' className={styles.textarea } onChange={contact.handleChange}
+        value={contact.values.message}></textarea>
 
         <button className={styles.btn} type="submit">Submit</button>
+        
       </div>
+      </form> 
     </main>
   </div>
   )
