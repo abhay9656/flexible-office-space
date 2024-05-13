5a2569c93607f4a06f21d95b6e9de8216a87c995
abhay9656 <abhay9656gupta@gmail.com>
2024-05-13 15:15:39 +0530

    Update user profile form fields in page.jsx

--- frontend/src/app/user/profile/page.jsx
+++ frontend/src/app/user/profile/page.jsx
@@ -7,8 +7,7 @@ const profile = () => {
   const userProfile = useFormik({
     initialValues: {
       image: "",
-      firstName: "",
-      lastName: "",
+      name: "",
       email: "",
       password: "",
       confirmPassword: "",
@@ -127,20 +126,12 @@ const profile = () => {
                 <div className="sm:col-span-9">
                   <div className="sm:flex">
                     <input
-                      id="firstName"
+                      id="name"
                       onChange={userProfile.handleChange}
                       value={userProfile.values.firstName}
                       type="text"
                       className="py-2 px-3 pe-11 bg-white border-2 block w-full border-gray-200 shadow-sm -mt-px -ms-px first:rounded-t-lg last:rounded-b-lg sm:first:rounded-s-lg sm:mt-0 sm:first:ms-0 sm:first:rounded-se-none sm:last:rounded-es-none sm:last:rounded-e-lg text-sm relative focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-                      placeholder="First Name"
-                    />
-                    <input
-                      type="text"
-                      id="lastName"
-                      onChange={userProfile.handleChange}
-                      value={userProfile.values.lastName}
-                      className="py-2 px-3 pe-11 bg-white border-2 block w-full border-gray-200 shadow-sm -mt-px -ms-px first:rounded-t-lg last:rounded-b-lg sm:first:rounded-s-lg sm:mt-0 sm:first:ms-0 sm:first:rounded-se-none sm:last:rounded-es-none sm:last:rounded-e-lg text-sm relative focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-                      placeholder="Last Name"
+                      placeholder="Enter your name"
                     />
                   </div>
                 </div>
@@ -177,14 +168,14 @@ const profile = () => {
                   <div className="space-y-2">
                     <input
                       id="password"
-                      type="text"
+                      type="password"
                       onChange={userProfile.handleChange}
                       value={userProfile.values.password}
                       className="py-2 px-3 pe-11 bg-white border-2 block w-full border-gray-200 shadow-sm rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
                       placeholder="Enter your password"
                     />
                     <input
-                      type="text"
+                      type="password"
                       id="ConfirmPassword"
                       onChange={userProfile.handleChange}
                       value={userProfile.values.ConfirmPassword}
