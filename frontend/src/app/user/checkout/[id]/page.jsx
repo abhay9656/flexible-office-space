86ef7d01683df59bddc9f9ade8ee82ece0a6554b
abhay9656 <abhay9656gupta@gmail.com>
2024-06-02 22:21:53 +0530

    Refactor feedbackModel.js to remove commented out code, add createdAt field, and update name field type

--- frontend/src/app/user/checkout/[id]/page.jsx
+++ frontend/src/app/user/checkout/[id]/page.jsx
@@ -72,169 +72,22 @@ const CheckOut = () => {
   return (
     <>
       <>
-        <div className="flex flex-col items-center border-b bg-white py-4 sm:flex-row sm:px-10 lg:px-20 xl:px-32">
-          <a href="#" className="text-2xl font-bold text-gray-800">
-            Checkout
-          </a>
-          <div className="mt-4 py-2 text-xs sm:mt-0 sm:ml-auto sm:text-base">
-            <div className="relative">
-              <ul className="relative flex w-full items-center justify-between space-x-2 sm:space-x-4">
-                <li className="flex items-center space-x-3 text-left sm:space-x-4">
-                  <a
-                    className="flex h-6 w-6 items-center justify-center rounded-full bg-emerald-200 text-xs font-semibold text-emerald-700"
-                    href="#"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-4 w-4"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M5 13l4 4L19 7"
-                      />
-                    </svg>
-                  </a>
-                  <span className="font-semibold text-gray-900">Shop</span>
-                </li>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4 text-gray-400"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9 5l7 7-7 7"
-                  />
-                </svg>
-                <li className="flex items-center space-x-3 text-left sm:space-x-4">
-                  <a
-                    className="flex h-6 w-6 items-center justify-center rounded-full bg-gray-600 text-xs font-semibold text-white ring ring-gray-600 ring-offset-2"
-                    href="#"
-                  >
-                    2
-                  </a>
-                  <span className="font-semibold text-gray-900">Shipping</span>
-                </li>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4 text-gray-400"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9 5l7 7-7 7"
-                  />
-                </svg>
-                <li className="flex items-center space-x-3 text-left sm:space-x-4">
-                  <a
-                    className="flex h-6 w-6 items-center justify-center rounded-full bg-gray-400 text-xs font-semibold text-white"
-                    href="#"
-                  >
-                    3
-                  </a>
-                  <span className="font-semibold text-gray-500">Payment</span>
-                </li>
-              </ul>
-            </div>
-          </div>
+        <div className="   bg-white py-4 sm:flex-row sm:px-10 lg:px-20 xl:px-32">
+          <h1 href="#" className="text-3xl text-center font-bold text-gray-800">
+          Check<span className="text-blue-700">Out</span>
+          </h1>
+          
         </div>
         <div className="grid sm:px-10 lg:grid-cols-2 lg:px-20 xl:px-32">
           <div className="px-4 pt-8">
-            <p className="text-xl font-medium">Order Summary</p>
-            <p className="text-gray-400">
-              Check your items. And select a suitable shipping method.
-            </p>
-            <div className="mt-8 space-y-3 rounded-lg border bg-white px-2 py-4 sm:px-6">
-              {/* {cartItems.map((item, index) => (
-                <div className="flex flex-col rounded-lg bg-white sm:flex-row">
-                  <img
-                    className="m-2 h-24 w-28 rounded-md border object-cover object-center"
-                    src={`${process.env.NEXT_PUBLIC_API_URL}/${item.images[0]}`}
-                    alt=""
-                  />
-                  <div className="flex w-full flex-col px-4 py-4">
-                    <span className="font-semibold">
-                      {item.pname}
-                    </span>
-                    <span className="float-right text-gray-400">
-                      {item.category}
-                    </span>
-                    <p className="text-lg font-bold">₹{item.pprice} x {item.quantity}</p>
-                  </div>
-                </div>
-              ))} */}
-            </div>
-            <p className="mt-8 text-lg font-medium">Shipping Methods</p>
-            <form className="mt-5 grid gap-6">
-              <div className="relative">
-                <input
-                  className="peer hidden"
-                  id="radio_1"
-                  type="radio"
-                  name="radio"
-                  defaultChecked=""
-                />
-                <span className="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white" />
-                <label
-                  className="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4"
-                  htmlFor="radio_1"
-                >
-                  <img
-                    className="w-14 object-contain"
-                    src="/images/naorrAeygcJzX0SyNI4Y0.png"
-                    alt=""
-                  />
-                  <div className="ml-5">
-                    <span className="mt-2 font-semibold">Cash On Deleviry</span>
-                    <p className="text-slate-500 text-sm leading-6">
-                      Delivery: 2-4 Days
-                    </p>
-                  </div>
-                </label>
-              </div>
-              <div className="relative">
-                <input
-                  className="peer hidden"
-                  id="radio_2"
-                  type="radio"
-                  name="radio"
-                  defaultChecked=""
-                />
-                <span className="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white" />
-                <label
-                  className="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4"
-                  htmlFor="radio_2"
-                >
-                  <img
-                    className="w-14 object-contain"
-                    src="/images/oG8xsl3xsOkwkMsrLGKM4.png"
-                    alt=""
-                  />
-                  <div className="ml-5">
-                    <span className="mt-2 font-semibold">Fedex Delivery</span>
-                    <p className="text-slate-500 text-sm leading-6">
-                      Delivery: 2-4 Days
-                    </p>
-                  </div>
-                </label>
-              </div>
-            </form>
+           
+           <div>
+            <img src="/payment.svg" alt="" />
+           </div>
+           
           </div>
           <div className="mt-10 bg-gray-50 px-4 pt-8 lg:mt-0">
-            <p className="text-xl font-medium">Payment Details</p>
+            <p className="text-xl text-black font-medium">Payment Details</p>
             <p className="text-gray-400">
               Complete your order by providing your payment details.
             </p>
